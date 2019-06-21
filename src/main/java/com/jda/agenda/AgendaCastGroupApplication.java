a95206c57fff0aa1e188b17e417ccfdd0837656a
jaderdeandrade <jaderdeandrade@gmail.com>
2019-06-21 14:39:25 -0300

    agenda cast group

---
+++ src/main/java/com/jda/agenda/AgendaCastGroupApplication.java
@@ -0,0 +1,13 @@
+package com.jda.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCastGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCastGroupApplication.class, args);
+	}
+
+}
